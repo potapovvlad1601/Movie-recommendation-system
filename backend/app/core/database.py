2a07ce2cd321612be7f3047df77c5e9db88b9e84
potapovvlad1601 <potapovvlad1601@gmail.com>
2026-09-20 18:39:52 +0300

    add hash_password and verify_password functions

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -23,3 +23,11 @@ SessionLocal = sessionmaker(
 
 class Base(DeclarativeBase):
     pass
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from sqlalchemy import text
+from sqlalchemy.orm import Session
 
-from app.core.database import engine
+from app.core.database import engine, get_db
+from app.core.security import hash_password
+
+from app.models.user import User
+from app.schemas.user import UserCreate, UserResponse
 
 
 app = FastAPI(
@@ -36,6 +41,22 @@ async def database_health_check():
         "result": result.scalar()
     }
 
+@app.post("/api/users", response_model=UserResponse)
+def create_user(
+    user_data: UserCreate,
+    db: Session = Depends(get_db)
+):
+    user = User(
+        username=user_data.username,
+        email=user_data.email,
+        password_hash=hash_password(user_data.password)
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
 
 # Найти все процессы tasklist | findstr python
 # Найти netstat -ano | findstr :8000
